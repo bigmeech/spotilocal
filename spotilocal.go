dc65d3541877092d5d021cb1e13689f3c22d19fa
Larry Eliemenye <larry.eliemenye@tuitravel.com>
2017-03-20 21:03:05 +0000

    - added support for fetching csrf token from remote spotify server

--- spotilocal.go
+++ spotilocal.go
@@ -5,21 +5,28 @@ import (
 	http "net/http"
 	logger "github.com/op/go-logging"
 	time "time"
-	"encoding/json"
+	json "encoding/json"
+	ioutil "io/ioutil"
 )
 
 const ORIGIN_URL string = "https://open.spotify.com"
+const LOCAL_URL string = "https://open.spotify.com"
 const DEFAULT_PORT int = 4370
 const MIN_PORT int = DEFAULT_PORT
 const MAX_PORT int = 4380;
 const RANDOM_STRING_LEN int = 10
 const LOCAL_URL_PREFIX string = ".spotilocal.com"
 
+const TOKEN_PATH string = "/token"
+const CSRF_TOKEN_PATH string = "/simplecsrf/token.json"
+
 var log = logger.MustGetLogger("Spotilocal")
 
 type Spotilocal struct {
 	Host string
 	Port string
+	T Token
+	CToken CSRFToken
 }
 
 //generates a random string as subdomain
@@ -34,33 +41,51 @@ func getSubDomain(str_len int) string {
 }
 
 // Detect connectable ports by trying port ranges 4370 to 4380
-func Connect () {
+func Connect () Spotilocal {
+	var token Token
+	var csrf_token CSRFToken
+
 	randomString := getSubDomain(RANDOM_STRING_LEN)
 	log.Debug("Connecting to " + randomString)
-	decodedToken := GetToken()
-	log.Debug(decodedToken.T)
+
+	//TYPES ASSERTIONS YO!!!!!!! FROM SUMTING TO ANYTING BACK TO SOMETHING.
+	decodedToken := getJSON(ORIGIN_URL + TOKEN_PATH, token).(Token)
+	decodedCSRFToken := getJSON(ORIGIN_URL + CSRF_TOKEN_PATH, csrf_token).(CSRFToken);
+
+	log.Debug(decodedToken)
+
+	return Spotilocal{ T: decodedToken, CToken: decodedCSRFToken }
 }
 
 // Gets token from remote
-func GetToken () Token {
-	var token Token
-	response, resp_err := http.Get(ORIGIN_URL+ "/token")
+func getJSON (url string, target interface{}) interface{} {
+	response, resp_err := http.Get(url)
 
 	if resp_err != nil {
 		panic(resp_err.Error())
 	}
 	defer response.Body.Close()
 
-	decode_error := json.NewDecoder(response.Body).Decode(&token)
+	raw_body, io_error := ioutil.ReadAll(response.Body)
+	if io_error != nil {
+		panic(io_error)
+	}
+	json_data := []byte(raw_body)
+
+	decode_error := json.Unmarshal(json_data, &target)
 	if decode_error != nil {
 		panic(decode_error.Error())
 	}
-	return token
+	return target
 }
 
 
+type CSRFToken struct {
+	Token string `json: "token"`
+}
+
 type Token struct {
-	T string
+	T string `json: "t"`
 }
 
 // start local server
